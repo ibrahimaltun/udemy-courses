1f6b824df6127ff21809b3319fd3b83d832735cd
ibrahimaltun <bykzist34@hotmail.com>
2026-05-19 22:30:29 +0300

    add nodes to manage the flow

---
+++ ai_tools-llm_rag_langchain_vectorstore/E_LangGraphSection/graph/nodes/retrieve.py
@@ -0,0 +1,11 @@
+from ingestion import retriever
+from graph.state import GraphState
+from typing import Dict, Any
+
+
+def retrieve(state: GraphState) -> Dict[str, Any]:
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
